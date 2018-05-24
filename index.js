95c796187995796339a7f02168b73c7fb82f87dd
Brent Lewis <brent.lewis08@gmail.com>
2018-05-23 22:54:24 -0500

    Add list/reds/white functions

--- index.js
+++ index.js
@@ -1,80 +1,96 @@
 const styles = {
-	RED: 'red',
-	WHITE: 'white',
-	ROSE: 'rose'
+    RED: 'red',
+    WHITE: 'white',
+    ROSE: 'rose'
 };
 
 const wines = [
-	{
-		name: 'Cabernet Sauvignon',
-		style: styles.RED,
-		isSparkling: false
-	},
-	{
-		name: 'Champagne',
-		style: styles.WHITE,
-		isSparkling: true
-	},
-	{
-		name: 'Chardonnay',
-		style: styles.WHITE,
-		isSparkling: false
-	},
-	{
-		name: 'Chianti',
-		style: styles.RED,
-		isSparkling: false
-	},
-	{
-		name: 'Malbec',
-		style: styles.RED,
-		isSparkling: false
-	},
-	{
-		name: 'Petite Sirah',
-		style: styles.RED,
-		isSparkling: false
-	},
-	{
-		name: 'Pinot Grigio',
-		style: styles.WHITE,
-		isSparkling: false
-	}, 
-	{
-		name: 'Pinot Noir',
-		style: styles.RED,
-		isSparkling: false
-	},
-	{
-		name: 'Riesling',
-		style: styles.RED,
-		isSparkling: false
-	}, 
-	{
-		name: 'Rose (dry)',
-		style: styles.ROSE,
-		isSparkling: false
-	}, 
-	{
-		name: 'Rose Champagne',
-		style: styles.RED,
-		isSparkling: true
-	},
-	{
-		name: 'Sauvignon Blanc',
-		style: styles.WHITE,
-		isSparkling: false
-	}, 
-	{
-		name: 'Syrah',
-		style: styles.RED,
-		isSparkling: false
-	}, 
-	{
-		name: 'Zinfandel',
-		style: styles.ROSE,
-		isSparkling: false
-	}
+    {
+        name: 'Cabernet Sauvignon',
+        style: styles.RED,
+        isSparkling: false
+    },
+    {
+        name: 'Champagne',
+        style: styles.WHITE,
+        isSparkling: true
+    },
+    {
+        name: 'Chardonnay',
+        style: styles.WHITE,
+        isSparkling: false
+    },
+    {
+        name: 'Chianti',
+        style: styles.RED,
+        isSparkling: false
+    },
+    {
+        name: 'Malbec',
+        style: styles.RED,
+        isSparkling: false
+    },
+    {
+        name: 'Petite Sirah',
+        style: styles.RED,
+        isSparkling: false
+    },
+    {
+        name: 'Pinot Grigio',
+        style: styles.WHITE,
+        isSparkling: false
+    }, 
+    {
+        name: 'Pinot Noir',
+        style: styles.RED,
+        isSparkling: false
+    },
+    {
+        name: 'Riesling',
+        style: styles.RED,
+        isSparkling: false
+    }, 
+    {
+        name: 'Rose (dry)',
+        style: styles.ROSE,
+        isSparkling: false
+    }, 
+    {
+        name: 'Rose Champagne',
+        style: styles.RED,
+        isSparkling: true
+    },
+    {
+        name: 'Sauvignon Blanc',
+        style: styles.WHITE,
+        isSparkling: false
+    }, 
+    {
+        name: 'Syrah',
+        style: styles.RED,
+        isSparkling: false
+    }, 
+    {
+        name: 'Zinfandel',
+        style: styles.ROSE,
+        isSparkling: false
+    }
 ];
 
-module.exports = wines;
+const toName = wine => {
+    return wine.name;
+};
+
+const reds = () => {
+    return wines.filter(wine => wine.style === styles.RED).map(toName);
+};
+
+const whites = () => {
+    return wines.filter(wine => wine.style === styles.WHITE).map(toName);
+};
+
+module.exports = {
+    list: () => wines,
+    reds: reds,
+    whites: whites
+};
