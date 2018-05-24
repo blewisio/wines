7bd10b225b56cf63490202976bb23b2837295be1
Brent Lewis <brent.lewis08@gmail.com>
2018-05-23 22:15:02 -0500

    Initialize :tada:

---
+++ index.js
@@ -0,0 +1,80 @@
+const styles = {
+	RED: 'red',
+	WHITE: 'white',
+	ROSE: 'rose'
+};
+
+const wines = [
+	{
+		name: 'Cabernet Sauvignon',
+		style: styles.RED,
+		isSparkling: false
+	},
+	{
+		name: 'Champagne',
+		style: styles.WHITE,
+		isSparkling: true
+	},
+	{
+		name: 'Chardonnay',
+		style: styles.WHITE,
+		isSparkling: false
+	},
+	{
+		name: 'Chianti',
+		style: styles.RED,
+		isSparkling: false
+	},
+	{
+		name: 'Malbec',
+		style: styles.RED,
+		isSparkling: false
+		 dry: true, fruity: true },
+	{
+		name: 'Petite Sirah',
+		style: styles.RED,
+		isSparkling: false
+	},
+	{
+		name: 'Pinot Grigio',
+		style: styles.WHITE,
+		isSparkling: false
+	}, 
+	{
+		name: 'Pinot Noir',
+		style: styles.RED,
+		isSparkling: false
+	},
+	{
+		name: 'Riesling',
+		style: styles.RED,
+		isSparkling: false
+	}, 
+	{
+		name: 'Rose (dry)',
+		style: styles.ROSE,
+		isSparkling: false
+	}, 
+	{
+		name: 'Rose Champagne',
+		style: styles.RED,
+		isSparkling: true
+	},
+	{
+		name: 'Sauvignon Blanc',
+		style: styles.WHITE,
+		isSparkling: false
+	}, 
+	{
+		name: 'Syrah',
+		style: styles.RED,
+		isSparkling: false
+	}, 
+	{
+		name: 'Zinfandel',
+		style: styles.ROSE,
+		isSparkling: false
+	}
+];
+
+module.exports = wines;
